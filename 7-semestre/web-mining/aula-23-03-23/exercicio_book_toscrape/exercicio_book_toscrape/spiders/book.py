bd2a43862debbc478ad98bacab1bc3d0c7a6144a
Lucas Santos <lucas.parkl@outlook.com>
2023-03-23 22:20:59 -0300

    adicionando exercicio scrapy

---
+++ 7-semestre/web-mining/aula-23-03-23/exercicio_book_toscrape/exercicio_book_toscrape/spiders/book.py
@@ -0,0 +1,23 @@
+import scrapy
+
+# Arthur Vinicius Santos Silva RA: 1903665
+# Larissa Ionafa RA: 1903166
+# Lucas da Silva Santos RA: 1904209
+# Rafael Serino Kiss RA: 1903107
+
+class BookSpider(scrapy.Spider):
+    name = "book"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com/catalogue/category/books/romance_8/index.html"]
+
+    def parse(self, response):
+        categoria = response.css('h1::text').get()
+
+        for livros in response.css(".col-lg-3 "):            
+                    yield {
+                        "categoria": categoria,
+                        "Titulo": livros.css('.col-lg-3 a::text').get(),
+                        "preco" : livros.css('.col-lg-3 .price_color::text').get(),
+                        "estoque": livros.css(".availability").get().split("\n")[3].strip()      
+                    }
+        pass
