e6a988d4045870b8cd8f0742c07f5bd1de4844f0
Lucas Santos <lucas.parkl@outlook.com>
2023-04-13 19:33:23 -0300

    exemplo e desagio aula 16-03-23

--- 7-semestre/web-mining/aula-16-03-23/exemplo.py
+++ 7-semestre/web-mining/aula-16-03-23/exemplo.py
@@ -14,16 +14,19 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
-
 navegador.get('http://www.ssp.sp.gov.br/transparenciassp/')
+
 navegador.find_element(By.ID, 'cphBody_btnRouboVeiculo').click()
+
 navegador.find_element(By.XPATH, '//*[@id="cphBody_lkAno22"]').click()
 
 for i in range(1, 13):
     item_mes = f'//*[@id="cphBody_lkMes{i}"]'
+    print("mes  --------------------------- " + item_mes)
    
     try:
         navegador.find_element(By.XPATH, item_mes).click()
         botao_exportar = WebDriverWait(navegador, 6000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="cphBody_ExportarBOLink"]')))
     finally:
         botao_exportar.click()
+        print("cabô")
